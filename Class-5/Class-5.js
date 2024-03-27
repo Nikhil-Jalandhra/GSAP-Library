b2c88ec221a9de53be59a55ec45ab87e63381774
Nikhil-Jalandhra <jneek36@gmail.com>
2024-03-27 17:10:25 +0530

    adding loader feature in app learning gsap

--- Class-5/Class-5.js
+++ Class-5/Class-5.js
@@ -1,3 +1,30 @@
+ var tl = gsap.timeline()
+
+ function time(){
+    var a = 0
+    setInterval(() => {
+        a = a + Math.floor(Math.random()*10)
+        if(a<100){
+            document.querySelector("#loader h1").innerHTML = a+"%"
+        }else{
+            a=100
+            document.querySelector("#loader h1").innerHTML = a+"%"
+        }
+    }, 50);
+ }
+ 
+ tl.to("#loader h1",{
+    delay: 0.5,
+    duration:1,
+    onStart:time()
+ })
+ 
+ tl.to("#loader",{
+    top:"-100%",
+    delay: 0./5,
+    duration: 1.5
+ })
+
 gsap.to("#page1 p",{
     transform: "translateX(-300%)",
     scrollTrigger:{
